b42fb57e9824dc67f86668b76a42eaca227b7d5c
Leandro A. Noskoski <noskoski@gmail.com>
2019-12-19 20:40:44 -0300

    2.0

--- collor.py
+++ collor.py
@@ -13,43 +13,25 @@ __maintainer__ 	= "Leandro Abelin Noskoski"
 __email__ 	= "leandro@alternativalinux.net"
 __status__ 	= "Production"
 
-import sys, re, random
+import sys, re, random, argparse
 
 i = 1
 attrange = [0] + list(range(7 , 0, -1))
 fgrange = list(range(30, 37 + 1)) + list(range(90,97 + 1)) + [39]
-args = []
-
-def print_usage():
-    print("""
-Usage: collor.py [ options ..]  colorizetext1 colorizetext2 colorizetext3 ...
-
-Examples:
-    cat /var/log/syslog | collor.py  colorizetext1 colorizetext2 colorizetext3
-    tail -f /var/log/syslog | collor.py -r colorizetext1 colorizetext2 colorizetext3
-
-Options:
-    -r              Randomize colors
-    -o              Only print Matches
-    -h or --help    This Message
-    -s              Colorize similars ( colorize equal words with same random color )
-
-
-Version: """ + __version__ + """
-""")
-
-
+bgrange = list(range(40, 47 + 1)) + list(range(100,107 + 1)) + [49]
+_hi = []
 
 class item:
-    def __init__(self,nome=None,fg=None,att=None):
+    def __init__(self,nome=None,fg=None,att=None,bg=None):
         self.nome = nome
         self.fg = random.choice(fgrange)
+        self.bg = random.choice(bgrange)
         self.att = random.choice(attrange)
         self.qtd = 1
-        self.cnome = '\033[' + str(self.att) + ';' + str(self.fg) + 'm' + str(self.nome) +  '\033[0m'
+        self.cnome = '\x1B[' + str(self.att) + ';' + str(self.bg) + ';' + str(self.fg) + 'm' + str(self.nome) +  '\x1B[0m'
 
     def print(self):
-        return("nome:" + str(self.cnome) + " fg:" + str(self.fg) + " att:" + str(self.att) + " qtd:" + str(self.qtd))
+        return("nome:" + str(self.cnome) + " fg:" + str(self.fg) + " bg:" + str(self.bg) + " att:" + str(self.att) + " qtd:" + str(self.qtd))
 
 
 
@@ -61,10 +43,7 @@ class db:
 
     def add(self, _str, ign=None):
         if ign:
-            #splitted_str = _str.split(" ");
-            
             splitted_str = self._split(_str)
-
             for slice in splitted_str:
                 if len(slice) >= self.min and len(slice) < self.max:
                     tt = self.check(slice)
@@ -84,15 +63,39 @@ class db:
         sys.stdout.write(str(len(self.words)))
 
     def check(self,nome):
-        for _x in self.words:
-            if  _x.nome == nome:
-                _x.qtd += 1
-                return(self.words.index(_x))
+        if '\x1B[' not in nome: ## bypass existent color
+            for _x in self.words:
+                if  _x.nome == nome:
+                    _x.qtd += 1
+                    return(self.words.index(_x))
         return(None)
 
     def _split(self,_str):
-        _grp = [ '\[.*?\]','\(.*?\)','\<.*?\>','\{.*?\}','[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+','[0-9]{2,4}-[0-9]{2}-[0-9]{2,4}','[0-2]{0,1}[0-9]{1}\:[0-9]{1,2}\:[0-9]{1,2}','http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+' ]
+
+        _grp = [""]
+        if "S" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('\[.*?\]')
+        if "P" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('\(.*?\)')
+        if "X" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('\<.*?\>')
+        if "Y" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('\{.*?\}')
+        if "T" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('[0-2]{0,1}[0-9]{1}\:[0-9]{1,2}\:[0-9]{1,2}')
+        if "E" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+        if "D" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('[0-9]{2,4}-[0-9]{2}-[0-9]{2,4}')
+        if "U" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+        if "I" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+        if "Q" in args.patterns[0] or "A" in args.patterns[0]:
+            _grp.append('"(.*?)"')
+            _grp.append('\'(.*?)\'')
         _arr = []
+#        print(_grp)
         for _i in _grp :
              _matches = re.findall(_i, _str)
              for _match in _matches:
@@ -106,33 +109,61 @@ class db:
 if __name__ == '__main__':
 
 
-    if len(sys.argv) <2  or ("-h" in sys.argv) or ("--help" in sys.argv) :
-        print_usage()
-        sys.exit(1)
 
-    if "-r" in sys.argv:
+    parser = argparse.ArgumentParser(description='Process some integers.')
+
+    parser.add_argument("-r", "--random",
+                help="set you need random colors",
+                action='store_true')
+    parser.add_argument("-o", "--only",
+                help="print only match strings",
+                action='store_true')
+    parser.add_argument("-p", "--patterns",
+                help="Search for patterns ADTUEIPSXY = \n( A=ALL, D=Date, T=Time, U=Urls, E=Emails, I=Ips, P=(), S=[], X=<>, Y={}, Q=\"\" or '' )",
+                metavar='pattern chars',
+                type=str,
+                nargs='+')
+    parser.add_argument("-H", "--highlight",
+                help=" word's for highlight",
+                metavar='word',
+                type=str,
+                nargs='+')
+
+    args = parser.parse_args()
+    print(args)
+
+#    if len(sys.argv) <2  or ("-h" in sys.argv) or ("--help" in sys.argv) :
+#        sys.exit(1)
+
+    if args.random:
         random.shuffle(fgrange)
+        random.shuffle(bgrange)
         random.shuffle(attrange)
 
-    colordb = db(4,60)
+    colordb = db(1,600)
+    i=0
 
-    for att in attrange:
-        for clfg in fgrange :
-            if i < len(sys.argv):
-                if (not re.match("^-r$",sys.argv[i]) ) and (not re.match("^-o$",sys.argv[i]) ) and (not re.match("^-s$",sys.argv[i]) ):
-                    args.append([sys.argv[i],clfg,att])
-            else:
-                break
-            i += 1
+    if ( args.highlight==None and args.only==False and args.patterns==None ):
+        args.patterns="A"
+
+    if args.highlight:
+        for att in attrange:
+            for clbg in bgrange :
+                for clfg in fgrange :
+                    if i < len(args.highlight):
+                            _hi.append([args.highlight[i],clfg,att,clbg])
+                    else:
+                        break
+                    i += 1
 
     for line in sys.stdin:
         _have=0
-        for arg in args:
-            if None != re.match("(.*)" + re.escape(arg[0]) + "(.*)",line,re.M|re.I):
+        for hi in _hi:
+            if None != re.match("(.*)" + re.escape(hi[0]) + "(.*)",line,re.M|re.I):
                 _have=1
-            line = re.sub(re.escape(arg[0]),'\033[' + str(arg[2]) + ';'+str(arg[1])+'m' + arg[0] +  '\033[0m', line)
+            line = re.sub(re.escape(hi[0]),'\033[' + str(hi[2]) + ';' + str(hi[3]) + ';' + str(hi[1])+'m' + hi[0] +  '\033[0m', line)
 
-        if "-s" in sys.argv:
+        if args.patterns and len(args.patterns):
             _s=True
         else:
             _s=False
